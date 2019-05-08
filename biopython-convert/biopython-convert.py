de9dfec8e40d987005cd1c6d6d31af1d0538fafc
Nolan Woods <nolan_w@sfu.ca>
2019-05-08 10:52:15 -0700

    Add JMESPath record query

--- biopython-convert/biopython-convert.py
+++ biopython-convert/biopython-convert.py
@@ -4,10 +4,12 @@ import itertools
 import getopt
 import gffutils
 from gffutils import biopython_integration
+import JMESPathGen
 import sys
 
-usage = "Use: biopython-convert.py [-s] [-v] input_file input_type output_file output_type\n"\
-        "\t-s Split records into seperate files\n"\
+usage = "Use: biopython-convert.py [-s] [-v] [-f JMESPath] input_file input_type output_file output_type\n" \
+        "\t-s Split records into seperate files\n" \
+        "\t-q JMESPath to select records. Must return list of SeqIO records. Root is list of input SeqIO records.\n" \
         "\t-v Print version and exit\n"
 
 def append_filename(path, s):
@@ -25,9 +27,10 @@ def append_filename(path, s):
 
 if __name__ == '__main__':
     split = False
+    jpath = None
     # Parse arguments
     try:
-        opts, args = getopt.gnu_getopt(sys.argv[1:], 'vs')
+        opts, args = getopt.gnu_getopt(sys.argv[1:], 'vsq:')
         for opt, val in opts:
             if opt == '-v':
                 import __version
@@ -36,6 +39,8 @@ if __name__ == '__main__':
                 exit(0)
             elif opt == '-s':
                 split = True
+            elif opt == '-q':
+                jpath = val
 
     except getopt.GetoptError as err:
         print("Argument error(", err.opt, "): ", err.msg, file=sys.stderr)
@@ -51,10 +56,10 @@ if __name__ == '__main__':
     output_path = args[2]
     output_type = args[3]
 
-    with open(input_path, "rU") as input:
-        if input_type not in gff_types and output_type not in gff_types:
-            SeqIO.convert(sys.stdin, args[1], sys.stdout, args[1])
-            exit(0)
+    with open(input_path, "r") as input:
+        #if input_type not in gff_types and output_type not in gff_types:
+        #    SeqIO.convert(sys.stdin, args[1], sys.stdout, args[1])
+        #    exit(0)
 
         if input_type in gff_types:
             db = gffutils.create_db(input, ":memory:", merge_strategy="create_unique")
@@ -70,13 +75,17 @@ if __name__ == '__main__':
 
         output = open(append_filename(output_path, ".0") if split else output_path, "w")
 
+        if jpath:
+            input_records = JMESPathGen.search(jpath, input_records)
+
         for i, record in enumerate(input_records):
-            for feature in record.features:
-                if output_type in gff_types:
+            # TODO allow objects other than SeqRecord, transform to SeqRecord or handle special output (like if output format == txt|json, pretty print object)
+            if output_type in gff_types:
+                for feature in record.features:
                     print(biopython_integration.from_seqfeature(feature), file=output)
-                else:
-                    SeqIO.write(record, output, args[3])
+            else:
+                SeqIO.write(record, output, args[3])
             if split:
                 output.close()
-                output = open(append_filename(output_path, "." + (i+1)))
+                output = open(append_filename(output_path, "." + str(i+1)))
 
