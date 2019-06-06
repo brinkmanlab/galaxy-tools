164d4c3c8e7e7459fe90dab39d8b8141c4908177
Nolan Woods <nolan_w@sfu.ca>
2019-06-06 11:28:33 -0700

    Add bioperl compatibility script

---
+++ biopython-convert/bioperl_compat.py
@@ -0,0 +1,12 @@
+import sys
+from .biopython_convert import get_args, convert
+from Bio.SeqIO.InsdcIO import _InsdcWriter
+
+# Quote anticodon qualifiers
+_InsdcWriter.FTQUAL_NO_QUOTE = tuple(v for v in _InsdcWriter.FTQUAL_NO_QUOTE if v != 'anticodon')
+
+if __name__ == '__main__':
+    input_path, input_type, output_path, output_type, jpath, split, stats = get_args(sys.argv[1:])
+
+    with open(input_path, "r") as input:
+        convert(input, input_type, output_path, output_type, jpath, split, stats)
