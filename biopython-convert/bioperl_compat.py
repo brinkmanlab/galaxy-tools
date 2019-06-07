2b4903febb9780fa03d7342ee8a06c3a85c59045
Nolan Woods <nolan_w@sfu.ca>
2019-06-07 13:28:14 -0700

    Add transl_except to list of compat qualifiers

--- biopython-convert/bioperl_compat.py
+++ biopython-convert/bioperl_compat.py
@@ -3,7 +3,7 @@ from biopython_convert import get_args, convert
 from Bio.SeqIO.InsdcIO import _InsdcWriter
 
 # Quote anticodon qualifiers
-_InsdcWriter.FTQUAL_NO_QUOTE = tuple(v for v in _InsdcWriter.FTQUAL_NO_QUOTE if v != 'anticodon')
+_InsdcWriter.FTQUAL_NO_QUOTE = tuple(v for v in _InsdcWriter.FTQUAL_NO_QUOTE if v not in ['anticodon', 'transl_except'])
 
 if __name__ == '__main__':
     input_path, input_type, output_path, output_type, jpath, split, stats = get_args(sys.argv[1:])
