08e2c5556ef8789f4ccb675c4548f093455cbafd
Nolan Woods <nolan_w@sfu.ca>
2020-11-06 11:02:10 -0800

    Quote builds file path

--- microbedb/builds.sql
+++ microbedb/builds.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS builds_temp;
 CREATE TEMP TABLE builds_temp(dbkey unique, label);
 .mode csv
 .separator '	'
-.import $__app__.config.builds_file_path builds_temp
+.import '$__app__.config.builds_file_path' builds_temp
 INSERT OR REPLACE INTO builds_temp (dbkey, label)
 SELECT (r.rep_accnum || '_' || r.rep_version)                                           AS dbkey,
     (REPLACE(r.definition, ', complete genome.', '') || ' [' || r.rep_accnum || '.' || r.rep_version ||
@@ -15,5 +15,5 @@ WHERE genomeproject.version_id = (SELECT MAX(version_id) FROM version)
     AND r.rep_type = 'chromosome';
 .mode list
 .separator '	'
-.once $__app__.config.builds_file_path
+.once '$__app__.config.builds_file_path'
 SELECT dbkey, label FROM builds_temp ORDER BY dbkey;
